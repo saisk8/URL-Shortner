ae0d332b48ccf220b53bd2a71a758d4305e19ede
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-05-27 11:48:12 +0530

    Handle all cases

--- server.js
+++ server.js
@@ -81,4 +81,19 @@ app.get('/:short', (request, response) => {
   });
 });
 
+// Respond not found to all the wrong routes
+app.use((req, res) => {
+  res.status(404);
+  res.type('txt').send('Not found');
+});
+
+// Error Middleware
+app.use((err, req, res) => {
+  if (err) {
+    res.status(err.status || 500)
+      .type('txt')
+      .send(err.message || 'SERVER ERROR');
+  }
+});
+
 app.listen(process.env.PORT || 3000);
