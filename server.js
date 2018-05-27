8dd21ff6045505b43841bcc61dc39dbfd10727b4
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-05-27 13:03:47 +0530

    Add check for valid url

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const shortid = require('shortid');
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$&');
 const app = express();
 
+function checkURL(url) {
+  const regForUrl = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  return (regForUrl.test(url));
+}
+
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
   extends: true,
@@ -34,7 +40,7 @@ app.get('/short/:url(*)', (request, response) => {
           original_url: url,
           short_url: process.env.HOST + doc.short,
         });
-      } else {
+      } else if (checkURL(url)) {
         const short = shortid.generate();
         const newDocument = {
           url,
@@ -51,6 +57,8 @@ app.get('/short/:url(*)', (request, response) => {
           original_url: url,
           short_url: process.env.HOST + short,
         });
+      } else {
+        response.sendFile(`${__dirname}/views/error.html`);
       }
     });
   });
